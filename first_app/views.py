b34e0e484e2d5ab1a1e9b1418e3c18d4378dba8d
Mashael Alotaibi <mashael.alotaibi.ph@gmail.com>
2019-07-26 23:21:43 +0300

    about page added and function of all(home,login,contact)

--- first_app/views.py
+++ first_app/views.py
@@ -1,2 +1,16 @@
 from django.shortcuts import render
-# Create your views here.
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('home')
+
+def about(request):
+    return render (request, 'about.html')
+
+def login(request):
+    return HttpResponse('login')
+
+
+def contact(request):
+    return HttpResponse('contact')
