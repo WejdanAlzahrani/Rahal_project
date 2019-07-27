51317bff930ed5cbdaa1c9684f516a581c2e313d
Wejdan <437000017@imamm.org>
2019-07-27 18:14:27 +0300

    blog model added with title,Type,content and publication_date columns

--- first_app/models.py
+++ first_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class blog(models.Model):
+    TYPE_CHOICES=[
+        ('advice','Advice'),
+        ('story','Story'),
+        (None,'Choose')
+        ]
+
+    title=models.CharField(max_length=100)
+    Type=models.CharField(max_length=6,choices=TYPE_CHOICES)
+    content=models.TextField()
+    publication_date=models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
