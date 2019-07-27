84abfbd7d31775827d1522c49947739aff328e2c
Wejdan <437000017@imamm.org>
2019-07-27 20:44:51 +0300

    readers column added to blog model

--- first_app/models.py
+++ first_app/models.py
@@ -13,6 +13,6 @@ class blog(models.Model):
     Type=models.CharField(max_length=6,choices=TYPE_CHOICES)
     content=models.TextField()
     publication_date=models.DateField(auto_now_add=True)
-
+    readers=models.CharField(max_length=10,default='0')
     def __str__(self):
         return self.title
