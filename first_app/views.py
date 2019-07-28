4fb99572a7296e86820e2edb1da147e47f0919aa
Nouf Aljarallah <n.a.s.m.j@hotmail.com>
2019-07-28 13:46:40 +0300

    contact form, contact.html and contact url  added

--- first_app/views.py
+++ first_app/views.py
@@ -1,2 +1,38 @@
-from django.shortcuts import render
-# Create your views here.
+from django.shortcuts import render ,reverse
+from django.http import  HttpResponseRedirect
+from .form import ContactForm
+from django.contrib import messages 
+
+
+def contact(request):
+    form=ContactForm()
+    if request.method=='POST':
+        form =ContactForm(request.POST)
+        if form.is_valid():
+            name=form.cleaned_data['name']
+            email=form.cleaned_data ['email']
+            body=form.cleaned_data['body']
+            messages.success(request,'Thank you')
+            return HttpResponseRedirect(reverse('contact'))
+
+    data={
+        'form':form
+    }
+    return render(request,'contact.html',data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
