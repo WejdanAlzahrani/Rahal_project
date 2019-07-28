e897f7757022b1b10e8865986acf713debc47e69
Mashael Alotaibi <mashael.alotaibi.ph@gmail.com>
2019-07-28 13:08:08 +0300

    about added

--- first_app/views.py
+++ first_app/views.py
@@ -2,15 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def home(request):
-    return HttpResponse('home')
-
 def about(request):
     return render (request, 'about.html')
 
-def login(request):
-    return HttpResponse('login')
 
 
-def contact(request):
-    return HttpResponse('contact')
+
