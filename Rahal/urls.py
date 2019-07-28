36aa8c34ebec0f75d65153258313d2c4c0c93ae5
Wejdan <437000017@imamm.org>
2019-07-28 14:40:52 +0300

    home view , home.html and url added

--- Rahal/urls.py
+++ Rahal/urls.py
@@ -19,5 +19,6 @@ from first_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/',views.home, name='home'),
     path ('about/', views.about, name='about'),
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def home(request):
+    data={}
+    return render(request, 'home.html',data)
+
 
 def about(request):
     return render (request, 'about.html')
