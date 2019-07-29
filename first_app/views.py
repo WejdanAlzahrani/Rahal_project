716988f19ca5b3eedd66f208f56655fc2ecbf33e
Wejdan <437000017@imamm.org>
2019-07-29 15:21:15 +0300

    contact view and html added

--- first_app/views.py
+++ first_app/views.py
@@ -1,14 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from .forms import ContactForm
+from django.contrib import messages
+
 
 def home(request):
-    data={}
-    return render(request, 'home.html',data)
+    data = {}
+    return render(request, "home.html", data)
 
 
 def about(request):
-    return render (request, 'about.html')
+    return render(request, "about.html")
 
 
+def contact(request):
+    form = ContactForm()
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data["name"]
+            email = form.cleaned_data["email"]
+            body = form.cleaned_data["body"]
+            messages.success(request, "Thank you")
+            return HttpResponseRedirect(reverse("contact"))
 
-
+    data = {"form": form}
+    return render(request, "contact.html", data)
